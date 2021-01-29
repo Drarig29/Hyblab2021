84a3e5d6486bcab3fdc4c75545c2217e618f45ab
Rouli <nittram36@gmail.com>
2021-01-29 18:28:38 +0100

    Add credits and score pages background, struggled to make buttons

--- velo-b/public/js/slide2.js
+++ velo-b/public/js/slide2.js
@@ -7,3 +7,14 @@ registerSlide("page-finale", function () {
         loop: true
     });
 });
+
+
+registerSlide("score", function () {
+    overrideAnim({
+        targets: '#img-fini',
+        scale: 1.2,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        loop: true
+    });
+});
